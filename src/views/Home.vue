dff7ee1747e0aad76ddadaba47c118f6f3a54f4e
a105111207 <a22101602@gmail.com>
2025-08-15 22:48:13 +0800

    update Home loading images animated icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,7 +40,10 @@ const items = [
     </template>
 
     <template #fallback>
-      <div>Loading Pictures...</div>
+      <div>
+        <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+        <span class="sr-only">Loading...</span>
+      </div>
     </template>
   </Suspense>
 </template>
