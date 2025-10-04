5be774329e7324535dac8733123f14b8343386e1
a105111207 <a22101602@gmail.com>
2025-10-04 13:51:06 +0800

    feat: implement redirect logic and create 404 page for improved navigation

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chiro/', // Replace 'chiro' with your repository name
+  base: '/', // Replace 'chiro' with your repository name
   plugins: [
     vue(),
     vueDevTools(),
